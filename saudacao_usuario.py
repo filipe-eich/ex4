f197dbd530b4d48ca9a6eeda6a2c9e48076b3d5a
filipe-eich <filipe.eich@gmail.com>
2025-02-25 20:53:40 -0300

    Add files via upload

---
+++ saudacao_usuario.py
@@ -0,0 +1,23 @@
+"""
+Programa: saudacao_usuario
+Descrição: Pergunta o nome do usuário e faz uma saudação
+Autor: Filipe Eich
+Data: 25/02/2025
+
+"""
+
+# Alocação de memória
+user = ""|
+frase =
+
+# Entrada de dados
+
+
+user = input("\nQual o seu nome: ")
+
+# Processamento de dados
+frase = f"Oi, {user}!"
+
+# Saída de dados'
+
+print(frase)
